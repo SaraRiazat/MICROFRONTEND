8c58770d86ac0e97c3452bcc1959715ebcabdcb7
s.riazat <s.riazat@AT60209>
2021-12-11 12:54:49 +0330

    Updates

---
+++ precache-manifest.3ed50718fa644f73b6a9b4b1e94b6934.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b006bcd36351d545a9c1457fe4a6673e",
+    "url": "/MICROFRONTEND/index.html"
+  },
+  {
+    "revision": "7b47006c62f4e8a86c92",
+    "url": "/MICROFRONTEND/static/css/main.css"
+  },
+  {
+    "revision": "7b47006c62f4e8a86c92",
+    "url": "/MICROFRONTEND/static/js/main.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/MICROFRONTEND/static/js/main.js.LICENSE.txt"
+  }
+]);
